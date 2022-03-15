2fbf979e126db41515f0320fe2fce8646e29be8e
lelenglet <lenglet.lea03@gmail.com>
2022-03-15 17:49:27 +0100

    Version initiale

---
+++ crypto.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+void PermutationTableau(){
+    // algo de fisher yates
+    //
+    // arguments possibles : alphabet non permuté :type char
+    //                       longueur de la chaine : type int
+    //
+    // boucle for i | n-1 -> 1{
+    //j = entier aléatoire entre 0 et i
+    // permutation valeur place j et i
+    // }
+
+}
+
+
+
+
+
+
+
+
+int main(){
+    char alphabet[28];
+    strcpy(alphabet,"ABCDEFGHIJKLMNOPQRSTUVWXYZ "); //alphabet de départ
+    char permutation[28];
+    strcpy(permutation,alphabet); // alphabet qui sera permuté
+
+
+
+    
+    
+
+
+
+
+
+
+
+return 0;
+}
